ae4fd9415c2053bd581a19b617c8b0657250542d
Jose Vera <joeyvera@outlook.com>
2020-10-14 13:19:47 +0200

    Add project files.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wincolmem;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestChangeColour()
+        {
+            Color testColour = Color.FromName("Red");
+            testColour = Form1.ChangeColour(testColour);
+            Assert.AreEqual("Green", testColour.Name);
+        }
+    }
+}
---
+++ Wincolmem/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Wincolmem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemoryLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.HiScoresButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MemoryLabel
+            // 
+            this.MemoryLabel.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.MemoryLabel.Location = new System.Drawing.Point(38, 88);
+            this.MemoryLabel.Name = "MemoryLabel";
+            this.MemoryLabel.Size = new System.Drawing.Size(242, 64);
+            this.MemoryLabel.TabIndex = 0;
+            this.MemoryLabel.Text = "Memory";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Debussy", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(97, 242);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(122, 40);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // HiScoresButton
+            // 
+            this.HiScoresButton.Font = new System.Drawing.Font("Debussy", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HiScoresButton.Location = new System.Drawing.Point(97, 298);
+            this.HiScoresButton.Name = "HiScoresButton";
+            this.HiScoresButton.Size = new System.Drawing.Size(122, 40);
+            this.HiScoresButton.TabIndex = 2;
+            this.HiScoresButton.Text = "Hi Scores";
+            this.HiScoresButton.UseVisualStyleBackColor = true;
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Font = new System.Drawing.Font("Debussy", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuitButton.Location = new System.Drawing.Point(97, 357);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(122, 40);
+            this.QuitButton.TabIndex = 3;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Debussy", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(61, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 56);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "C";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(131, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 56);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "l";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(96, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 56);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "o";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.label5.ForeColor = System.Drawing.Color.Purple;
+            this.label5.Location = new System.Drawing.Point(189, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 56);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "u";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.label6.ForeColor = System.Drawing.Color.Cyan;
+            this.label6.Location = new System.Drawing.Point(224, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 56);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "r";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Debussy", 39.75F);
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(150, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 56);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "o";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.HiScoresButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.MemoryLabel);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Colour Memory for Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+
+            this.components = new System.ComponentModel.Container();
+            this.mainMenuTimer = new System.Windows.Forms.Timer(this.components);
+
+            this.mainMenuTimer.Enabled = true;
+            this.mainMenuTimer.Tick += new System.EventHandler(this.mainMenuTimer_Tick);
+
+
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MemoryLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button HiScoresButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+
+
+
+    }
+}
+
